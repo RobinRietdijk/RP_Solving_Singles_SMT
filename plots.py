99c4f2bdbe3222f0d20158d79ffec7b012623c94
Robin Rietdijk <robinrietdijk@kabelfoon.net>
2025-12-04 12:00:53 +0100

    Add science plots

--- plots.py
+++ plots.py
@@ -1,5 +1,8 @@
 import os
 import matplotlib.pyplot as plt
+import scienceplots
+
+plt.style.use(['science','ieee'])
 
 def _plot_puzzleruntime(results):
     by_size = {}
@@ -27,7 +30,6 @@ def _plot_puzzleruntime(results):
             by_solver[solver][e["puzzle"]] = e["elapsed"]
 
         plt.figure()
-        plt.style.use(['science'])
         for index, solver in enumerate(solver_order):
             if solver not in by_solver:
                 continue
@@ -76,7 +78,6 @@ def _plot_puzzlestatistic(results, statistic, y_label):
             by_solver[solver][e["puzzle"]] = stat
 
         plt.figure()
-        plt.style.use(['science'])
         for index, solver in enumerate(solver_order):
             if solver not in by_solver:
                 continue
@@ -140,7 +141,6 @@ def _plot_avgruntime(results):
             continue
 
         plt.figure()
-        plt.style.use(['science'])
         x = list(range(len(solver_labels)))
         plt.bar(x, avg_per_solver)
         plt.xticks(x, solver_labels, rotation=45, ha='right')
@@ -183,7 +183,6 @@ def _plot_avgstatistic(results, statistic, y_label):
             continue
 
         plt.figure()
-        plt.style.use(['science'])
         x = list(range(len(solver_labels)))
         plt.bar(x, avg_per_solver)
         plt.xticks(x, solver_labels, rotation=45, ha='right')
@@ -226,7 +225,6 @@ def _plot_scalingavgruntime(results):
     solvers = sorted({solver for (_, solver) in sums.keys()})
 
     plt.figure()
-    plt.style.use(['science'])
     for index, solver in enumerate(solvers):
         z = 10 + (len(solvers)-index)
         x_sizes = []
@@ -267,7 +265,6 @@ def _plot_scalingavgstatistic(results, statistic, y_label, plot_name):
     solvers = sorted({solver for (_, solver) in sums.keys()})
 
     plt.figure()
-    plt.style.use(['science'])
     for index, solver in enumerate(solvers):
         z = 10 + (len(solver)-index)
         x_sizes = []
