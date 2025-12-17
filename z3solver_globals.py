4bc2f3a0cad397498f6fab8fea3473b4a6bc5c34
Robin Rietdijk <robin@rjrietdijk.com>
2025-12-17 16:41:09 +0100

    Fixed consistency in constraints

--- z3solver_globals.py
+++ z3solver_globals.py
@@ -1,41 +1,41 @@
 from z3 import *
 
 # Redundant constraint to make sure there is at least n / 2 in each row and column
-def least_whites(s: Solver, colored: list[list[BoolRef]], n: int) -> None:
+def least_whites(s: Solver, colored: list, puzzle: list, n: int) -> None:
     min_white = n//2
     for i in range(n):
         s.add(AtLeast(*[Not(colored[i][j]) for j in range(n)], min_white))
         s.add(AtLeast(*[Not(colored[j][i]) for j in range(n)], min_white))
 
 # Redundant constraint to make sure there is at most (n / 2) + 1 blacks in each row and column
-def most_blacks(s: Solver, colored: list[list[BoolRef]], n: int) -> None:
+def most_blacks(s: Solver, colored: list, puzzle: list, n: int) -> None:
     max_black = (n//2)+1
     for i in range(n):
         s.add(AtMost(*[colored[i][j] for j in range(n)], max_black))
         s.add(AtMost(*[colored[j][i] for j in range(n)], max_black))
 
 # Redundant constraint for pairs, making all other occurences of those values black
-def pair_isolation(s: Solver, colored: list[list[BoolRef]], grid: list[list[int]], n: int) -> None:
+def pair_isolation(s: Solver, colored: list, puzzle: list, n: int) -> None:
     if n < 4:
         return
     
     for i in range(n):
         for j in range(n-1):
-            if grid[i][j] == grid[i][j+1]:
+            if puzzle[i][j] == puzzle[i][j+1]:
                 for k in range(n):
-                    if k not in (j-1, j, j+1, j+2) and grid[i][j] == grid[i][k]:
+                    if k not in (j-1, j, j+1, j+2) and puzzle[i][j] == puzzle[i][k]:
                         s.add(colored[i][k])
             
-            if grid[j][i] == grid[j+1][i]:
+            if puzzle[j][i] == puzzle[j+1][i]:
                 for k in range(n):
-                    if k not in (j-1, j, j+1, j+2) and grid[j][i] == grid[k][i]:
+                    if k not in (j-1, j, j+1, j+2) and puzzle[j][i] == puzzle[k][i]:
                         s.add(colored[k][i])
 
 # Implementation of pattern 7 according to chapter 3.3 of Hitori Solver
-def close_isolation(s: Solver, colored: list[list[BoolRef]], grid: list[list[int]], n: int) -> None:
+def close_isolation(s: Solver, colored: list, puzzle: list, n: int) -> None:
     for i in range(n):
         for j in range(n-2):
-            if grid[i][j] == grid[i][j+2]:
+            if puzzle[i][j] == puzzle[i][j+2]:
                 conditions = []
                 if i > 0:
                     conditions.append(colored[i-1][j+1])
@@ -47,12 +47,12 @@ def close_isolation(s: Solver, colored: list[list[BoolRef]], grid: list[list[int
                 condition = And(*conditions) if len(conditions) > 1 else conditions[0]
 
                 for k in range(n):
-                    if k not in (j, j+2) and grid[i][k] == grid[i][j]:
+                    if k not in (j, j+2) and puzzle[i][k] == puzzle[i][j]:
                         s.add(Implies(condition, colored[i][k]))
 
     for j in range(n):
         for i in range(n-2):
-            if grid[i][j] == grid[i+2][j]:
+            if puzzle[i][j] == puzzle[i+2][j]:
                 conditions = []
                 if j > 0:
                     conditions.append(colored[i+1][j-1])
@@ -64,10 +64,10 @@ def close_isolation(s: Solver, colored: list[list[BoolRef]], grid: list[list[int
                 condition = And(*conditions) if len(conditions) > 1 else conditions[0]
 
                 for k in range(n):
-                    if k not in (i, i+2) and grid[k][j] == grid[i][j]:
+                    if k not in (i, i+2) and puzzle[k][j] == puzzle[i][j]:
                         s.add(Implies(condition, colored[k][j]))
 
-def bridges(s: Solver, colored: list[list[BoolRef]], n: int) -> None:
+def white_bridges(s: Solver, colored: list, puzzle: list, n: int) -> None:
     for i in range(n-1):
         whites_in_row1 = Or(*[Not(colored[i][j]) for j in range(n)])
         whites_in_row2 = Or(*[Not(colored[i+1][j]) for j in range(n)])
--- z3solver_locals.py
+++ z3solver_locals.py
@@ -1,7 +1,7 @@
 from z3 import *
 
 # Redundant constraint to make sure evert white cell has atleast one white neighbour
-def white_neighbours(s: Solver, colored: list[list[BoolRef]], n: int) -> None:
+def white_neighbours(s: Solver, colored: list, puzzle: list, n: int) -> None:
     for i in range(n):
         for j in range(n):
             neighbours = []
@@ -18,7 +18,7 @@ def white_neighbours(s: Solver, colored: list[list[BoolRef]], n: int) -> None:
                 s.add(Implies(Not(colored[i][j]), Or(*neighbours)))
 
 # Redundant constraint for corners
-def corner_close(s: Solver, colored: list[list[BoolRef]], n: int) -> None:
+def corner_close(s: Solver, colored: list, puzzle: list, n: int) -> None:
     s.add(Implies(colored[0][1], Not(colored[1][0])))
     s.add(Implies(colored[1][0], Not(colored[0][1])))
     s.add(Implies(colored[0][n-2], Not(colored[1][n-1])))
@@ -29,62 +29,62 @@ def corner_close(s: Solver, colored: list[list[BoolRef]], n: int) -> None:
     s.add(Implies(colored[n-2][n-1], Not(colored[n-1][n-2])))
 
 # Implementation of pattern 1 according to chapter 3.3 of Hitori Solver
-def sandwich_triple(s: Solver, colored: list[list[BoolRef]], grid: list[list[int]], n: int) -> None:
+def sandwich_triple(s: Solver, colored: list, puzzle: list, n: int) -> None:
     for i in range(n):
         for j in range(n):
             if i < n-2:
-                if grid[i][j] == grid[i+1][j] and grid[i][j] == grid[i+2][j]:
+                if puzzle[i][j] == puzzle[i+1][j] and puzzle[i][j] == puzzle[i+2][j]:
                     s.add(colored[i][j])
                     s.add(colored[i+2][j])
 
             if j < n-2:
-                if grid[i][j] == grid[i][j+1] and grid[i][j] == grid[i][j+2]:
+                if puzzle[i][j] == puzzle[i][j+1] and puzzle[i][j] == puzzle[i][j+2]:
                     s.add(colored[i][j])
                     s.add(colored[i][j+2])
 
-def sandwich_pair(s: Solver, colored: list[list[BoolRef]], grid: list[list[int]], n: int) -> None:
+def sandwich_pair(s: Solver, colored: list, puzzle: list, n: int) -> None:
     if n < 3:
         return 
     
     for i in range(n):
         for j in range(n-2):
-            if grid[i][j] == grid[i][j+2]:
+            if puzzle[i][j] == puzzle[i][j+2]:
                 s.add(Not(colored[i][j+1]))
-            if grid[j][i] == grid[j+2][i]:
+            if puzzle[j][i] == puzzle[j+2][i]:
                 s.add(Not(colored[j+1][i]))
 
 # Implementation of pattern 2 according to chapter 3.3 of Hitori Solver
-def triple_corner(s: Solver, colored: list[list[BoolRef]], grid: list[list[int]], n: int) -> None:
-    if (grid[0][0] == grid[0][1] and grid[0][0] == grid[1][0]):
+def triple_corner(s: Solver, colored: list, puzzle: list, n: int) -> None:
+    if (puzzle[0][0] == puzzle[0][1] and puzzle[0][0] == puzzle[1][0]):
         s.add(colored[0][0])
-    if (grid[0][n-1] == grid[0][n-2] and grid[0][n-1] == grid[1][n-1]):
+    if (puzzle[0][n-1] == puzzle[0][n-2] and puzzle[0][n-1] == puzzle[1][n-1]):
         s.add(colored[0][n-1])
-    if (grid[n-1][0] == grid[n-2][0] and grid[n-1][0] == grid[n-1][1]):
+    if (puzzle[n-1][0] == puzzle[n-2][0] and puzzle[n-1][0] == puzzle[n-1][1]):
         s.add(colored[n-1][0])
-    if (grid[n-1][n-1] == grid[n-1][n-2] and grid[n-1][n-1] == grid[n-2][n-1]):
+    if (puzzle[n-1][n-1] == puzzle[n-1][n-2] and puzzle[n-1][n-1] == puzzle[n-2][n-1]):
         s.add(colored[n-1][n-1])
 
 # Implementation of pattern 3 according to chapter 3.3 of Hitori Solver
-def quad_corner(s: Solver, colored: list[list[BoolRef]], grid: list[list[int]], n: int) -> None:
-    if (grid[0][0] == grid[0][1] and grid[0][0] == grid[1][0] and grid[0][0] == grid[1][1]):
+def quad_corner(s: Solver, colored: list, puzzle: list, n: int) -> None:
+    if (puzzle[0][0] == puzzle[0][1] and puzzle[0][0] == puzzle[1][0] and puzzle[0][0] == puzzle[1][1]):
         s.add(colored[0][0])
         s.add(colored[1][1])
         if n >= 3:
             s.add(Not(colored[0][2]))
             s.add(Not(colored[2][0]))
-    if (grid[0][n-1] == grid[0][n-2] and grid[0][n-1] == grid[1][n-1] and grid[0][n-1] == grid[1][n-2]):
+    if (puzzle[0][n-1] == puzzle[0][n-2] and puzzle[0][n-1] == puzzle[1][n-1] and puzzle[0][n-1] == puzzle[1][n-2]):
         s.add(colored[0][n-1])
         s.add(colored[1][n-2])
         if n >= 3:
             s.add(Not(colored[0][n-3]))
             s.add(Not(colored[2][n-1]))
-    if (grid[n-1][0] == grid[n-2][0] and grid[n-1][0] == grid[n-1][1] and grid[n-1][0] == grid[n-2][1]):
+    if (puzzle[n-1][0] == puzzle[n-2][0] and puzzle[n-1][0] == puzzle[n-1][1] and puzzle[n-1][0] == puzzle[n-2][1]):
         s.add(colored[n-1][0])
         s.add(colored[n-2][1])
         if n >= 3:
             s.add(Not(colored[n-3][0]))
             s.add(Not(colored[n-1][2]))
-    if (grid[n-1][n-1] == grid[n-1][n-2] and grid[n-1][n-1] == grid[n-2][n-1] and grid[n-1][n-1] == grid[n-2][n-2]):
+    if (puzzle[n-1][n-1] == puzzle[n-1][n-2] and puzzle[n-1][n-1] == puzzle[n-2][n-1] and puzzle[n-1][n-1] == puzzle[n-2][n-2]):
         s.add(colored[n-1][n-1])
         s.add(colored[n-2][n-2])
         if n >= 3:
@@ -92,75 +92,75 @@ def quad_corner(s: Solver, colored: list[list[BoolRef]], grid: list[list[int]], 
             s.add(Not(colored[n-1][n-3]))
 
 # Implementation of pattern 4 according to chapter 3.3 of Hitori Solver
-def triple_edge_pair(s: Solver, colored: list[list[BoolRef]], grid: list[list[int]], n: int) -> None:
+def triple_edge_pair(s: Solver, colored: list, puzzle: list, n: int) -> None:
     if n < 4:
         return
     
     for i in range(1, n-2):
-        if grid[0][i] == grid[0][i+1] and grid[1][i] == grid[1][i+1] and grid[2][i] == grid[2][i+1]:
+        if puzzle[0][i] == puzzle[0][i+1] and puzzle[1][i] == puzzle[1][i+1] and puzzle[2][i] == puzzle[2][i+1]:
             s.add(Not(colored[0][i-1]))
             s.add(Not(colored[1][i-1]))
             s.add(Not(colored[0][i+2]))
             s.add(Not(colored[1][i+2]))
-        if grid[n-1][i] == grid[n-1][i+1] and grid[n-2][i] == grid[n-2][i+1] and grid[n-3][i] == grid[n-3][i+1]:
+        if puzzle[n-1][i] == puzzle[n-1][i+1] and puzzle[n-2][i] == puzzle[n-2][i+1] and puzzle[n-3][i] == puzzle[n-3][i+1]:
             s.add(Not(colored[n-1][i-1]))
             s.add(Not(colored[n-2][i-1]))
             s.add(Not(colored[n-1][i+2]))
             s.add(Not(colored[n-2][i+2]))
-        if grid[i][0] == grid[i+1][0] and grid[i][1] == grid[i+1][1] and grid[i][2] == grid[i+1][2]:
+        if puzzle[i][0] == puzzle[i+1][0] and puzzle[i][1] == puzzle[i+1][1] and puzzle[i][2] == puzzle[i+1][2]:
             s.add(Not(colored[i-1][0]))
             s.add(Not(colored[i-1][1]))
             s.add(Not(colored[i+2][0]))
             s.add(Not(colored[i+2][1]))
-        if grid[i][n-1] == grid[i+1][n-1] and grid[i][n-2] == grid[i+1][n-2] and grid[i][n-3] == grid[i+1][n-3]:
+        if puzzle[i][n-1] == puzzle[i+1][n-1] and puzzle[i][n-2] == puzzle[i+1][n-2] and puzzle[i][n-3] == puzzle[i+1][n-3]:
             s.add(Not(colored[i-1][n-1]))
             s.add(Not(colored[i-1][n-2]))
             s.add(Not(colored[i+2][n-1]))
             s.add(Not(colored[i+2][n-2]))
 
 # Implementation of pattern 5 according to chapter 3.3 of Hitori Solver
-def double_edge_pair(s: Solver, colored: list[list[BoolRef]], grid: list[list[int]], n: int) -> None:
+def double_edge_pair(s: Solver, colored: list, puzzle: list, n: int) -> None:
     if n < 4:
         return
     
     for i in range(1, n-2):
-        if grid[0][i] == grid[0][i+1] and grid[1][i] == grid[1][i+1]:
+        if puzzle[0][i] == puzzle[0][i+1] and puzzle[1][i] == puzzle[1][i+1]:
             s.add(Not(colored[0][i-1]))
             s.add(Not(colored[0][i+2]))
-        if grid[n-1][i] == grid[n-1][i+1] and grid[n-2][i] == grid[n-2][i+1]:
+        if puzzle[n-1][i] == puzzle[n-1][i+1] and puzzle[n-2][i] == puzzle[n-2][i+1]:
             s.add(Not(colored[n-1][i-1]))
             s.add(Not(colored[n-1][i+2]))
-        if grid[i][0] == grid[i+1][0] and grid[i][1] == grid[i+1][1]:
+        if puzzle[i][0] == puzzle[i+1][0] and puzzle[i][1] == puzzle[i+1][1]:
             s.add(Not(colored[i-1][0]))
             s.add(Not(colored[i+2][0]))
-        if grid[i][n-1] == grid[i+1][n-1] and grid[i][n-2] == grid[i+1][n-2]:
+        if puzzle[i][n-1] == puzzle[i+1][n-1] and puzzle[i][n-2] == puzzle[i+1][n-2]:
             s.add(Not(colored[i-1][n-1]))
             s.add(Not(colored[i+2][n-1]))
 
-def close_edge(s: Solver, colored: list[list[BoolRef]], grid: list[list[int]], n: int) -> None:
+def close_edge(s: Solver, colored: list, puzzle: list, n: int) -> None:
     if n < 3:
         return
     for i in range(0, n):
         if i > 1:
-            if grid[1][i-2] == grid[1][i-1]:
+            if puzzle[1][i-2] == puzzle[1][i-1]:
                 s.add(Implies(colored[0][i], Not(colored[0][i-2])))
-            if grid[n-2][i-2] == grid[n-2][i-1]:
+            if puzzle[n-2][i-2] == puzzle[n-2][i-1]:
                 s.add(Implies(colored[n-1][i], Not(colored[n-1][i-2])))
-            if grid[i-2][1] == grid[i-1][1]:
+            if puzzle[i-2][1] == puzzle[i-1][1]:
                 s.add(Implies(colored[i][0], Not(colored[i-2][0])))
-            if grid[i-2][n-2] == grid[i-1][n-2]:
+            if puzzle[i-2][n-2] == puzzle[i-1][n-2]:
                 s.add(Implies(colored[i][n-1], Not(colored[i-2][n-1])))
         if i < n-2:
-            if grid[1][i+2] == grid[1][i+1]:
+            if puzzle[1][i+2] == puzzle[1][i+1]:
                 s.add(Implies(colored[0][i], Not(colored[0][i+2])))
-            if grid[n-2][i+2] == grid[n-2][i+1]:
+            if puzzle[n-2][i+2] == puzzle[n-2][i+1]:
                 s.add(Implies(colored[n-1][i], Not(colored[n-1][i+2])))
-            if grid[i+2][1] == grid[i+1][1]:
+            if puzzle[i+2][1] == puzzle[i+1][1]:
                 s.add(Implies(colored[i][0], Not(colored[i+2][0])))
-            if grid[i+2][n-2] == grid[i+1][n-2]:
+            if puzzle[i+2][n-2] == puzzle[i+1][n-2]:
                 s.add(Implies(colored[i][n-1], Not(colored[i+2][n-1])))
 
-def force_double_edge(s: Solver, colored: list[list[BoolRef]], grid: list[list[int]], n: int) -> None:
+def force_double_edge(s: Solver, colored: list, puzzle: list, n: int) -> None:
     if n < 3:
         return
     for i in range(0, n):
@@ -181,36 +181,36 @@ def force_double_edge(s: Solver, colored: list[list[BoolRef]], grid: list[list[i
             s.add(Implies(And(colored[i][n-1], colored[i+2][n-1]), Not(colored[i+1][n-2])))
 
 # Implementation of pattern 8 according to chapter 3.3 of Hitori Solver
-def border_close(s: Solver, colored: list[list[BoolRef]], grid: list[list[int]], n: int) -> None:
+def border_close(s: Solver, colored: list, puzzle: list, n: int) -> None:
     for i in range(n):
-        if grid[0][i] == grid[1][i]:
+        if puzzle[0][i] == puzzle[1][i]:
             if i > 1:
-                if grid[0][i] == grid[1][i-1]:
+                if puzzle[0][i] == puzzle[1][i-1]:
                     s.add(Implies(colored[0][i-2], colored[1][i]))
             if i < n-2:
-                if grid[0][i] == grid[1][i+1]:
+                if puzzle[0][i] == puzzle[1][i+1]:
                     s.add(Implies(colored[0][i+2], colored[1][i]))
          
-        if grid[n-1][i] == grid[n-2][i]:
+        if puzzle[n-1][i] == puzzle[n-2][i]:
             if i > 1:
-                if grid[n-1][i] == grid[n-2][i-1]:
+                if puzzle[n-1][i] == puzzle[n-2][i-1]:
                     s.add(Implies(colored[n-1][i-2], colored[n-2][i]))
             if i < n-2:
-                if grid[n-1][i] == grid[n-2][i+1]:
+                if puzzle[n-1][i] == puzzle[n-2][i+1]:
                     s.add(Implies(colored[n-1][i+2], colored[n-2][i]))
         
-        if grid[i][0] == grid[i][1]:
+        if puzzle[i][0] == puzzle[i][1]:
             if i > 1:
-                if grid[i][0] == grid[i-1][1]:
+                if puzzle[i][0] == puzzle[i-1][1]:
                     s.add(Implies(colored[i-2][0], colored[i][1]))
             if i < n-2:
-                if grid[i][0] == grid[i+1][1]:
+                if puzzle[i][0] == puzzle[i+1][1]:
                     s.add(Implies(colored[i+2][0], colored[i][1]))
         
-        if grid[i][n-1] == grid[i][n-2]:
+        if puzzle[i][n-1] == puzzle[i][n-2]:
             if i > 1:
-                if grid[i][n-1] == grid[i-1][n-2]:
+                if puzzle[i][n-1] == puzzle[i-1][n-2]:
                     s.add(Implies(colored[i-2][n-1], colored[i][n-2]))
             if i < n-2:
-                if grid[i][n-1] == grid[i+1][n-2]:
+                if puzzle[i][n-1] == puzzle[i+1][n-2]:
                     s.add(Implies(colored[i+2][n-1], colored[i][n-2]))
